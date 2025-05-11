37d607210d51dc27cc18718afb65d5ba731beb36
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-05-11 15:54:50 +0900

    game_comlete!!

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # walkwalk 
-おそらく一番作るのが簡単なゲーム（作成前）
+おそらく一番作るのが簡単なゲーム（作成前） </br>
+
+簡単だった（）</br>
+ただ，細かいところは結局参考にせざるを得なかった </br>
+
+[![Image from Gyazo](https://i.gyazo.com/82105f8f6a3ba8a323a1ab9056d9e71c.png)](https://gyazo.com/82105f8f6a3ba8a323a1ab9056d9e71c)
--- main.js
+++ main.js
@@ -8,6 +8,9 @@ let heroX = 1
 let heroY = 1
 let heroMove = [1, 0]
 
+let score = 0
+let messageDiv = null
+
 const render = () => {
     for (let y = 1; y < height + 1; y++) {
         for (let x = 1; x < width + 1; x++) {
@@ -15,12 +18,18 @@ const render = () => {
             cell.div.textContent = cell.text
         }
     }
+    messageDiv.textContent = `Score: ${score}`
 }
 
 const init = () => {
+    messageDiv = document.createElement('div')
+    document.body.appendChild(messageDiv)
+    messageDiv.style.position = 'relative'
+
     const container = document.createElement('div')
     document.body.appendChild(container)
     container.style.position = 'relative'
+
     for (let y = 0; y < height + 2; y++) {
         board[y] = []
         for (let x = 0; x < width + 2; x++) {
@@ -71,19 +80,29 @@ const init = () => {
 window.onload = async () => {
     init()
     for (let i = 0; i < 1000; i++) {
-        await new Promise(r => setTimeout(r, 400))
-        heroCell = board[heroY][heroX]
-        heroCell.text = '🐾'
-        heroX += heroMove[0]
-        heroY += heroMove[1]
-        heroCell = board[heroY][heroX]
-        if (heroCell.text) {
-            heroCell.text = '💥'
+        await new Promise(r => setTimeout(r, 2000))
+        // heroCell は用意するとかえって扱いづらいかもしれない？
+        // nextCell とか作ればいいのか，それはそれでわかりづらいか
+        //heroCell = board[heroY][heroX]
+        board[heroY][heroX].text = '🐾'
+        const [dx, dy] = heroMove
+        // ここで分けて受け取っておくと後でわかりやすい気がする
+        heroX += dx
+        heroY += dy
+        if (board[heroY][heroX].text) {
+            board[heroY - dy][heroX - dx].text = '💥'
             render()
             return;
         } else {
-            heroCell.text = '🐥'
+            board[heroY][heroX].text = '🐥'
             render()
         }
+
+        if (Math.random() < 0.2) {
+            const rx = Math.trunc(Math.random() * width + 1)
+            const ry = Math.trunc(Math.random() * height + 1)
+            board[ry][rx].text = '🌲'
+        }
+        score++;
     }
 }
